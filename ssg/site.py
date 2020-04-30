0c8e18c1486d21fae0dc1e4628f7c45403e3a6ce
Tom Bell <tjbell@gmail.com>
2020-04-30 14:40:20 -0600

    docs: published

---
+++ ssg/site.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+class Site():
+    def __init__(self,source,dest):
+        self.source = source.Path()
+        self.dest = dest.Path()
+
+    def create_dir(self):
+        directory =self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
