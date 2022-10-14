3a2a8cf73db3962fe9b863e1ba04f6e46dd9f16d
Salex1323 <franciscojavier.munozmoreno@alum.uca.es>
2022-10-14 11:43:42 +0200

    docs: published

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -13,16 +13,16 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path):
-        with open(path,"r") as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path,"w") as file:
+        with open(full_path, "w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, dest / path.relative_to(soutcre))
+        shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
     extensions = [".jpg",".png",".gif",".css",".html"]
