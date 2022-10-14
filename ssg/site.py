ae6a597f72a0eecccce95bfa1d33ebb83a5a878d
Salex1323 <franciscojavier.munozmoreno@alum.uca.es>
2022-10-14 11:12:45 +0200

    docs: published

--- ssg/site.py
+++ ssg/site.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-class Site():
-    def __init__(self,source,dest):
-        self.source = source.Path()
-        self.dest = dest.Path()
+class Site:
+    def __init__(self, source,  dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
 
-    def create_dir(self):
+    def create_dir(self,path):
         directory =self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
